c01f41eab95e1e87c84a0dac4452eeb9a88599f1
+ <+>
2022-06-10 06:23:56 +0300

    commit tree bf997f28c62fc9d0e1870e235f735d107848e08d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (names, strings){
+    return names.filter (function(name) {return name .toLOwerCase()=== strings.lowerCase()})
+}
+
+function fuzzyMatch(names, string){
+    return names.filter(function(name) {
+      return name.slice(0,2).toLowerCase() === string.toLowerCase();
+    })
+  }
+
+  function matchName(drivers, name){
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase()
+    )
+  }
